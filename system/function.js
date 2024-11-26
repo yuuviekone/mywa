55ce5c80322ea386735dc77e1a26a89418c92bc9
Yuuviekone <109352175+yuuviekone@users.noreply.github.com>
2024-11-26 09:12:32 +0700

    update system/function.js

--- system/function.js
+++ system/function.js
@@ -371,6 +371,19 @@ export function rand(length) {
     }
     return result
 }
+export function randnumber(length) {
+    const characters =
+        "0123456789"
+    let result = ""
+    const charactersLength = characters.length
+
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(
+            Math.floor(Math.random() * charactersLength)
+        )
+    }
+    return result
+}
 
 /* date */
 export function date(numer, timeZone = "") {
